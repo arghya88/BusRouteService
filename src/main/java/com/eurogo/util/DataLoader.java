3a12ae84c919fa22521ff698a075ab06a1a4dcd3
arghya88 <arghya88@gmail.com>
2017-03-07 01:36:39 +0530

    Added Tests

--- src/main/java/com/eurogo/util/DataLoader.java
+++ src/main/java/com/eurogo/util/DataLoader.java
@@ -12,19 +12,24 @@ public class DataLoader {
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 
 			String line = br.readLine();
-			int noOfRoutes=Integer.parseInt(line);
-			System.out.println("Total Number of Routes:"+noOfRoutes);
 			while ((line = br.readLine()) != null) {
 				String[] values=line.split(" ");
-				for(int i=1;i<values.length;i++){
-					graph.addVertex(Integer.parseInt(values[i]));
-				}
-				for(int i=1;i<values.length;i++){
-					if(i!=values.length-1)
-						graph.addEdge(Integer.parseInt(values[i]),Integer.parseInt(values[i+1]));
-				}
+				buildGraph(values);
 			}
 		}
 		return graph;
+	}
+	private static void buildGraph(String[] values) throws VertexNotFoundException {
+		for(int i=1;i<values.length;i++){
+			graph.addVertex(Integer.parseInt(values[i]));
+		}
+		for(int i=1;i<values.length;i++){
+			if(i!=values.length-1){
+				graph.addEdge(Integer.parseInt(values[i]),Integer.parseInt(values[i+1]));
+			}
+			if(i!=1){
+				graph.addEdge(Integer.parseInt(values[i]),Integer.parseInt(values[i-1]));
+			}
+		}
 	}
 }
--- src/test/java/com/eurogo/BusRouteApplicationTests.java
+++ src/test/java/com/eurogo/BusRouteApplicationTests.java
@@ -1,9 +1,9 @@
 package com.eurogo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +17,70 @@ public class BusRouteApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	@Test
+	public void testAddVertex(){
+		Graph<Integer> graph=new Graph<>();
+		assertTrue(graph.addVertex(0));
+		assertFalse(graph.addVertex(0));
+		assertTrue(graph.addVertex(1));
+	}
+	@Test
+	public void testAddEdge() throws VertexNotFoundException{
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		assertTrue(graph.addEdge(0, 1));
+	}
+	@Test(expected=VertexNotFoundException.class)
+	public void testAddEdgeThrowsException() throws VertexNotFoundException{
+		Graph<Integer> graph=new Graph<>();
+		assertTrue(graph.addEdge(0, 1));
+	}
+	
+	@Test
+	public void testRemoveVertex() {
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		assertTrue(graph.removeVertex(0));
+		assertFalse(graph.removeVertex(1));
+	}
+	@Test
+	public void testRemoveEdge() throws VertexNotFoundException{
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		assertTrue(graph.addEdge(0, 1));
+		assertTrue(graph.removeEdge(0,1));
+	}
+	@Test
+	public void testVetexCount(){
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		assertEquals(graph.vertexCount(),2);
+	}
+	@Test
+	public void testEdgeCount() throws VertexNotFoundException{
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		graph.addEdge(0, 1);
+		assertEquals(graph.edgeCount(),1);
+	}
+	@Test
+	public void testContainsVertex(){
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		assertTrue(graph.containsVertex(0));
+	}
+	@Test
+	public void testContainsEdge() throws VertexNotFoundException{
+		Graph<Integer> graph=new Graph<>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		graph.addEdge(0, 1);
+		assertTrue(graph.containsEdge(0, 1));
 	}
 	@Test 
 	public void testGraphHasPathBetweenVertex() throws VertexNotFoundException{
